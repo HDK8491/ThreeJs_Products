d4f4ebf327575d1db635a51eb7ef0a7fb319ea7b
Hardik Bhammar <hardikbhammar808@gmail.com>
2024-03-15 10:56:54 +0530

    mange

---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+import CustomButton from "./CustomButton";
+
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker";
+import FilePiker from "./FilePiker";
+import Tab from "./Tab";
+
+export {CustomButton,AIPicker,ColorPicker,FilePiker,Tab};
